2351f8f3a5591e355e162d225ff15db43a829603
Obinna Uko <obicodeman@yahoo.com>
2023-03-26 15:33:47 +0100

    design: sidebar containing websited copyright, and social links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { About, Contact, Work, Home } from "./pages"
+import { Body } from './styles';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Body>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/work' element={<Work />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+
+        </Routes>
+      </Body>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { Work } from "./Work";
+import { Home } from "./Home";
+import "../css/helpers.css"
+
+
+export {
+    About,
+    Contact,
+    Work,
+    Home
+}
