231d205464a28a58bd729b69089bd51b54e88256
Obinna Uko <obicodeman@yahoo.com>
2023-04-22 16:03:52 +0100

    revamp of portfolio website

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,6 @@
 
 export const About = () => {
     return (
-        <div>About</div>
+        <div>Obinna Ofe has been involved in the software industry for over 5 years, developing a complex line of business applications in various sectors. He has spent the last 5 years building single page applications using a wide range of JavaScript technologies, including Angular, ReactJS, and TypeScript.</div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,12 @@
 
-import React from 'react';
-import { Text, TextSmall, Slider, SideNav, Container, Wrapper } from '../styles';
-import { Icon } from '@iconify/react';
-import githubLogo from '@iconify/icons-radix-icons/github-logo';
-import { Link } from 'react-router-dom';
+import "./home.css";
 export const Home = () => {
     return (
         <>
 
-            <Slider></Slider>
 
-            <SideNav>
-                <Wrapper flex column center>
-                    <Wrapper flex column spaceBtw gap="2rem">
-                        <Link to={"//github.com/obicodeman555"} target="_blank" rel='non-referer'>
-                            <Text>
-                                <Icon icon={githubLogo} className="icon-svg" />
-                            </Text>
-                        </Link>
-                        <Link to={"//linkedin.com/in/obinna-ofe-8b07a8124"}>
-                            <Text>
-                                <Icon icon="entypo-social:linkedin-with-circle" className="icon-svg" />
-                            </Text>
-                        </Link>
-                    </Wrapper>
-                    <TextSmall className='font-sm'>
-                        &#169;/2023
-                    </TextSmall>
-                </Wrapper>
-            </SideNav>
-            <Container>
-                <Text>Hello</Text>
-                <Text>
-                    Flex
-                </Text>
-            </Container>
+
+
         </>
     )
 }
