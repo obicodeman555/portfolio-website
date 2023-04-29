0d7ded2313b66616825df41018689b5a2a6ec72c
Obinna Uko <obicodeman@yahoo.com>
2023-04-29 06:23:25 +0100

    landing page design complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,37 +9,64 @@ const About = () => {
             <header className="portfolio-header">
                 <Icon icon="ci:hamburger-lg" />
             </header>
-            <article>
-                Obinna Ofe has been involved in the software industry for
-                over 5 years, developing a complex line of business applications in various sectors.
-                He has spent the last 5 years building single page applications using a wide range of
-                JavaScript technologies, including Angular, ReactJS, and TypeScript.
+            <article className='about-me__text'>
+                <h1>
+                    <span>Good Morning,</span>
+                    <span>My name is Obinna Ofe.</span>
+                </h1>
+                <p>
+                    I, have been involved in the software industry for
+                    over 5 years, developing a complex line of business applications in various sectors.
+
+                </p>
+                <p>
+                    Furthermore, I have spent the last 5 years building single page applications using a wide range of
+                    JavaScript technologies, including Angular, ReactJS, VueJS, and TypeScript.
+                </p>
+                <p>
+                    Feel free to take a look at my latest projects on the &nbsp;
+                    <Link to="/work" className='about-link'>web portfolio page</Link>.
+                </p>
+
+                <div className='contact-me'>
+                    <Link to="mailto:obinnaukoofe@gmail.com">
+                        get in touch
+                    </Link>
+                </div>
             </article>
 
             <ul className='socials'>
                 <li>
-                    <Icon icon="pajamas:github" />
+                    <Link to="">
+                        <Icon icon="pajamas:github" />
+                    </Link>
                 </li>
                 <li>
-                    <Icon icon="jam:linkedin" />
+                    <Link to="">
+                        <Icon icon="jam:linkedin" />
+                    </Link>
                 </li>
                 <li>
-                    <Icon icon="ic:baseline-email" />
+                    <Link to="mailto:obinnaukoofe@gmail.com">
+                        <Icon icon="ic:baseline-email" />
+                    </Link>
                 </li>
             </ul>
 
-            <div>
-                follow on github
-            </div>
-            <div className='link-to-page'>
-                <Link to="#">portfolio</Link>
-            </div>
-
-            <div className='contact-me'>
-                <Link to="#">
-                    get in touch
+            <div className='github-follow'>
+                <Link to={""}>
+                    follow on github
                 </Link>
             </div>
+            <div className='link-to-page'>
+                <div className='inner'>
+                    <Link to="/work">
+                        <span>portfolio</span>
+                    </Link>
+                </div>
+            </div>
+
+
 
         </main>
     )
